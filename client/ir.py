b4fc2ea949e060a94f8dbe52662a9ce157ff323a
Thanoraj <muthulingamthanoraj@gmail.com>
2024-07-11 15:55:55 +0530

    ir testing added

--- client/ir.py
+++ client/ir.py
@@ -1,22 +1,31 @@
-import Jetson.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 # Pin Definitions
-ir_sensor_pin = 17  # Pin 11 on the header, GPIO17
+input_pin = 18  # BCM pin 18, BOARD pin 12
 
-# Pin Setup
-GPIO.setmode(GPIO.BCM)  # BCM pin-numbering scheme from Raspberry Pi
-GPIO.setup(ir_sensor_pin, GPIO.IN)  # IR sensor pin set as input
 
-try:
-    print("Starting IR sensor monitoring...")
-    while True:
-        if GPIO.input(ir_sensor_pin):
-            print("Object detected!")
-        else:
-            print("No object detected.")
-        time.sleep(0.5)  # Delay for half a second
-except KeyboardInterrupt:
-    print("Exiting gracefully.")
-finally:
-    GPIO.cleanup()  # Cleanup all GPIO
+def main():
+    prev_value = None
+
+    # Pin Setup:
+    GPIO.setmode(GPIO.BCM)  # BCM pin-numbering scheme from Raspberry Pi
+    GPIO.setup(input_pin, GPIO.IN)  # set pin as an input pin
+    print("Starting demo now! Press CTRL+C to exit")
+    try:
+        while True:
+            value = GPIO.input(input_pin)
+            if value != prev_value:
+                if value == GPIO.LOW:
+                    value_str = "Tracking human..."
+                else:
+                    value_str = "Human exited"
+                print(value_str)
+                prev_value = value
+            time.sleep(1)
+    finally:
+        GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    main()
